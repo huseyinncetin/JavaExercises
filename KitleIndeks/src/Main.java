ad02e1afe4d2cc3903f0563371059496bae54446
Huseyin CETIN <20ihhs20@gmail.com>
2022-10-13 19:01:05 +0300

    Ekleme

--- KitleIndeks/src/Main.java
+++ KitleIndeks/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double boy,kilo,indeks;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy = input.nextFloat();
+
+        System.out.println("Lütfen kilonuzu giriniz :");
+        kilo = input.nextFloat();
+
+        indeks = kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz :"+indeks);
     }
 }
