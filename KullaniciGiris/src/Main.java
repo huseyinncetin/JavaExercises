184f0373b9f6452330a27a33cf873975a000545d
Huseyin CETIN <20ihhs20@gmail.com>
2022-10-14 20:20:05 +0300

    Sifre kontrol eklendi

---
+++ KullaniciGiris/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int password = 5;
+        int yeniSifre;
+        int istek;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Parolanızı giriniz:");
+        password =input.nextInt();
+
+        if( password!=5){
+            System.out.println("Şifrenizi sıfırlamak ister misiniz:1/Evet 2/Hayır");
+            istek =input.nextInt();
+
+            if(istek==1){
+                Scanner yenile = new Scanner(System.in);
+                System.out.println("Şifreyi giriniz:");
+                yeniSifre = yenile.nextInt();
+
+                if(yeniSifre == 5){
+                    System.out.println("Şifre oluşturulamadı lütfen tekrar deneyin.");
+                }
+               else if((yeniSifre != 5 )) {
+                    System.out.println("Şifre oluşturuldu");
+                }
+            }
+        }
+    }
+}
